1f06826284b4773dafe7c426d5e013b5e892fc6b
milcho0604 <milcho0604@gmail.com>
2025-03-19 17:32:32 +0900

    feat: header test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Board from "./components/Board";
 import ExchangeRateCalculator from "./components/ExchangeRateCalculator";
+import Header from "./headerComponent/Header";
+import Main from "./components/Main";
 
 function App() {
   return (
     <div>
-
+      <Header />
       <ExchangeRateCalculator />
+      <Main />
+
     </div>
   );
 }
